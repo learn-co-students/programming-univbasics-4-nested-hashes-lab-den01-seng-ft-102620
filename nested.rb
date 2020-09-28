c9ec2292f6d6efd9a4b76b1ebbef7477d31f5be0
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-28 16:34:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,9 +79,18 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      },
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
       }
-    }
+ 		}
 
+# programmer_hash[:yukihiro_matsumoto] = {},
+# programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby",
+# programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP",
+# programmer_hash[:yukihiro_matsumoto][:languages][1] = "C",
+# programmer_hash
 end
 
 def changing_alan
@@ -104,6 +113,8 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +136,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 
 end
