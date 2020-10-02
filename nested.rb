0633c2e8f9ca0b8cd59a777c267bf1c6943e12b9
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-02 22:39:23 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +38,9 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:alan_kay][:known_for]
+    
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +87,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {}
+    
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    
+    programmer_hash
 
 end
 
@@ -103,6 +117,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -125,5 +143,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    
+    programmer_hash
 
 end
